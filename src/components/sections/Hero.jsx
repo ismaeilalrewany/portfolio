b46ffbf4936398f42815adc798a6cddb67b43075
ismaeil alrewany <ismailalrewany332@gmail.com>
2025-01-16 17:20:26 +0200

    Hero Section Prototype

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,7 +1,11 @@
 export const Hero = () => {
   return (
-    <section className="">
-      <h1>Hero</h1>
+    <section className="bg-gray-600 h-screen">
+      <div className="container mx-auto text-blue-500 h-full flex flex-col justify-center items-center">
+        <h1 className="text-[40px] lg:text-[96px] font-medium">Ismaeil Alrewany</h1>
+        <p className="text-[16px] lg:text-[32px]">I&apos;m a Front-end Developer</p>
+        <button type="button" className="py-2 px-4 border rounded-[20px] mt-3 md:mt-4 text-[14px] md:text-[16px]">Download CV</button>
+      </div>
     </section>
   )
 }
