8b40eef76674359eca18fd4e67b30aaecdc22083
ismaeil alrewany <ismailalrewany332@gmail.com>
2025-01-17 23:39:02 +0200

    Keydown, Wheel, and Touch added to Navigation

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,15 +1,72 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { NavContext } from "../../context/NavContext"
 import navbarData from "../../data/navbar"
 
 export const Navigation = () => {
-  const { setCurrentItem } = useContext(NavContext)
+  const { currentItem, setCurrentItem } = useContext(NavContext)
+  const [touchStart, setTouchStart] = useState(0)
 
   const handleSectionClick = item => {
     navbarData.navItems.forEach(navItem => navItem.active = false)
     item.active = true
     setCurrentItem(item)
   }
+
+  const handleKeyDown = (event) => {
+    let currentIndex = navbarData.navItems.findIndex(item => item.active)
+
+    if (event.key === "ArrowUp" && currentIndex > 0) {
+      handleSectionClick(navbarData.navItems[currentIndex - 1])
+    } else if (event.key === "ArrowDown" && currentIndex < navbarData.navItems.length - 1) {
+      handleSectionClick(navbarData.navItems[currentIndex + 1])
+    } else {
+      return
+    }
+  }
+
+  const handleWheel = (event) => {
+    let currentIndex = navbarData.navItems.findIndex(item => item.active)
+
+    if (event.deltaY < 0 && currentIndex > 0) {
+      handleSectionClick(navbarData.navItems[currentIndex - 1])
+    } else if (event.deltaY > 0 && currentIndex < navbarData.navItems.length - 1) {
+      handleSectionClick(navbarData.navItems[currentIndex + 1])
+    } else {
+      return
+    }
+  }
+
+  const handleTouchStart = (event) => {
+    setTouchStart(event.touches[0].clientY)
+  }
+
+  const handleTouchEnd = (event) => {
+    const currentIndex = navbarData.navItems.findIndex(item => item.active)
+    const touchEnd = event.changedTouches[0].clientY
+    const touchDifference = touchStart - touchEnd
+
+    if (touchDifference > 50 && currentIndex < navbarData.navItems.length - 1) {
+      handleSectionClick(navbarData.navItems[currentIndex + 1])
+    } else if (touchDifference < -50 && currentIndex > 0) {
+      handleSectionClick(navbarData.navItems[currentIndex - 1])
+    } else {
+      return
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown)
+    window.addEventListener("wheel", handleWheel)
+    window.addEventListener("touchstart", handleTouchStart)
+    window.addEventListener("touchend", handleTouchEnd)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+      window.removeEventListener("wheel", handleWheel)
+      window.removeEventListener("touchstart", handleTouchStart)
+      window.removeEventListener("touchend", handleTouchEnd)
+    }
+  }, [currentItem, touchStart])
 
   return (
     <nav className="fixed right-4 top-[50%] translate-y-[-50%]">
