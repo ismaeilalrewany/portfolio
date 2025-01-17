d929f86a501e694fe383798e1c2934f016752c47
ismaeil alrewany <ismailalrewany332@gmail.com>
2025-01-17 19:06:29 +0200

    Navbar Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,27 @@ import { Hero } from './components/sections/Hero'
 import { Skills } from './components/sections/Skills'
 import { Projects } from './components/sections/Projects'
 import { Footer } from './components/layout/Footer'
+import { useContext, useEffect } from 'react'
+import { NavContext } from './context/NavContext'
+import { useNavContext } from './hooks/useNavContext'
 
 export const App = () => {
+  const { item } = useContext(NavContext)
+  const { title, section } = useContext(NavContext)
+  const sections = { Hero, Skills, Projects, Footer }
+  const SectionComponent = sections[section] || Hero
+
+  useEffect(() => {
+    document.title = `Portfolio | ${title}`
+  }, [title])
+
   return (
     <main className="relative">
       <Header />
-      <Nav />
-      <Hero />
-      <Skills />
-      <Projects />
-      <Footer />
+      <useNavContext>
+        <Nav />
+        <SectionComponent />
+      </useNavContext>
     </main>
   )
 }
--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,11 +1,27 @@
+import { useContext } from "react"
+import { NavContext } from "../../context/NavContext"
+import { navItems } from "../../data/navbar"
+
 export const Nav = () => {
+  const { setItem } = useContext(NavContext)
+
+  const sectionHandler = (e) => {
+    const itemId = e.target.key
+    navItems.forEach(item => item.active = false)
+    navItems.forEach(item => {
+      if (item.id === itemId) {
+        item.active = true
+        setItem(itemId)
+      }
+    })
+  }
+
   return (
     <nav className="fixed right-4 top-[50%] translate-y-[-50%]">
       <ul className="list-none text-blue-500">
-        <li className="cursor-pointer mb-2">Hero</li>
-        <li className="cursor-pointer mb-2">Skills</li>
-        <li className="cursor-pointer mb-2">Projects</li>
-        <li className="cursor-pointer mb-2">Footer</li>
+        {navItems.map(item => (
+          <li key={item.id} className="cursor-pointer mb-2" onClick={sectionHandler}>{item.section}</li>
+        ))}
       </ul>
     </nav>
   )
