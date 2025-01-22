f9ceed48c4e6194e8d6c51e467624f95c85fa5ac
ismaeil alrewany <ismailalrewany332@gmail.com>
2025-01-23 01:22:34 +0200

    Header Features Automatic, Hour Theme, and Theme Icon

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,13 @@
+import SettingIcon from '../../assets/icons/setting-line.svg?react'
+
 export const Header = () => {
   return (
-    <header className="fixed top-4 left-[50%] translate-x-[-50%] text-blue-500 bg-white text-center py-2 px-4 rounded-[20px] border border-blue-500">
-      Header
+    <header className="fixed top-4 left-[50%] translate-x-[-50%] text-center">
+      <div className="w-10 h-10 rounded-full bg-white flex items-center justify-center cursor-pointer opacity-25 hover:opacity-100">
+        <SettingIcon className="w-8 h-8 fill-current text-blue-500" />
+      </div>
+      <div></div>
+      <div></div>
     </header>
   )
 }
