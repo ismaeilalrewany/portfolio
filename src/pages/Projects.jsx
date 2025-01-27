34dccbd244d24de57a1e0e3a8c98a70795f3a7dd
ismaeil alrewany <ismailalrewany332@gmail.com>
2025-01-27 06:17:15 +0200

    Center Cards in Phone Screens

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ export const Projects = () => {
     <section className="h-screen text-blue-500 bg-gray-600 font-NovaSquare pt-[60px] overflow-x-hidden  overflow-y-auto scrollbar-thin scrollbar-track-gray-400 scrollbar-thumb-slate-500">
       <div className="container px-2 mx-auto text-center">
         <h1 className="font-medium text-[28px] lg:text-[48px] font-Orbitron">Projects</h1>
-        <div className="grid grid-cols-1 gap-4 mt-4 lg:grid-cols-2">
+        <div className="grid place-items-center grid-cols-1 gap-4 mt-4 lg:grid-cols-2">
           {
             projectsData.projects.map(project => (
               <Card key={project.id} data={project} />
